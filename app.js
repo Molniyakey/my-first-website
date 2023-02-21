a0f0ef7bc600ee72296c3b519ca95fdd9dea210f
Molniyakey <Andreystav77@yandex.ru>
2023-02-21 17:42:41 +0300

    Start NEw Ficha

--- app.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.burger');
-const menu = document.querySelector('.mobile-nav');
-
-
-burger.addEventListener('click', (e) => {
-    menu.classList.toggle('hide')
-})
